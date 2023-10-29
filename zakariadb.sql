213a49b0b1be3ff2c0b2ed88787e06dabe64753e
ZAKARIA EL BIDALI <zakariael1212@gmail.com>
2023-10-29 23:43:39 +0000

    Update zakariadb.sql

--- zakariadb.sql
+++ zakariadb.sql
@@ -1,16 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 29, 2023 at 02:45 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -22,11 +12,9 @@ CREATE DATABASE zakariadb;
 USE zakariadb;
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `tickets`
 --
-
 CREATE TABLE `tickets` (
   `ticket_id` int(11) NOT NULL,
   `id` int(11) NOT NULL,
@@ -41,22 +29,18 @@ CREATE TABLE `tickets` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `users`
 --
-
 CREATE TABLE `users` (
   `Id` int(11) NOT NULL,
   `Username` varchar(200) DEFAULT NULL,
   `Email` varchar(200) DEFAULT NULL,
   `Password` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Indexes for dumped tables
 --
-
 --
 -- Indexes for table `tickets`
 --
